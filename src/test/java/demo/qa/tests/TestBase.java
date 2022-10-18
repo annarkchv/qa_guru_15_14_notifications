d265a3dc8d0c5f39e3681685a93c31fcca27d5b9
AnnaRkchv <rakchaevaa@gmail.com>
2022-10-18 13:35:10 +0300

    Add selenoid to configuration remote

--- src/test/java/demo/qa/tests/TestBase.java
+++ src/test/java/demo/qa/tests/TestBase.java
@@ -4,12 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
-
     @BeforeAll
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-
 }
