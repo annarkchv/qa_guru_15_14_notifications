d756cb2c392cc8f6902d0d8ba823f4a82ced449f
AnnaRkchv <rakchaevaa@gmail.com>
2022-10-18 14:50:05 +0300

    Add attachments

--- src/test/java/demo/qa/tests/TestBase.java
+++ src/test/java/demo/qa/tests/TestBase.java
@@ -2,16 +2,26 @@ package demo.qa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
     static void setUp() {
+//        Add steps to AllureReport
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachment() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
