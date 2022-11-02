a26b0b891d59eceadeb8c6d1369b9c8ad575b36a
AnnaRkchv <rakchaevaa@gmail.com>
2022-11-02 13:20:01 +0200

    add config files for notifications

--- src/test/java/demo/qa/tests/PracticeFormWithPageObjectsTest.java
+++ src/test/java/demo/qa/tests/PracticeFormWithPageObjectsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static demo.qa.tests.TestData.*;
 import static io.qameta.allure.Allure.step;
 
-public class PracticeFormWithPageObjectsTest extends TestBase{
+public class PracticeFormWithPageObjectsTest extends TestBase {
 
     PracticeFormPage practiceFormPage = new PracticeFormPage();
 
@@ -14,18 +14,18 @@ public class PracticeFormWithPageObjectsTest extends TestBase{
     void fillFormTest() {
 
         step("Fill form (all fields)", () -> {
-                    practiceFormPage.openPage()
-                            .setName(firstUserFirstName, firstUserLastName)
-                            .setMail(firstUserMail)
-                            .setGender(firstUserGender)
-                            .setNumber(firstUserNumber)
-                            .setBirthDate(firstUserDayOfMonth, firstUserMonth, firstUserYear)
-                            .setSubject(firstUserSubject)
-                            .setHobby(firstUserHobby)
-                            .uploadFile(userFile)
-                            .setCurrentAddress(firstUserAddress)
-                            .setStateAndCity(firstUserState, firstUserCity)
-                            .submitForm();
+            practiceFormPage.openPage()
+                    .setName(firstUserFirstName, firstUserLastName)
+                    .setMail(firstUserMail)
+                    .setGender(firstUserGender)
+                    .setNumber(firstUserNumber)
+                    .setBirthDate(firstUserDayOfMonth, firstUserMonth, firstUserYear)
+                    .setSubject(firstUserSubject)
+                    .setHobby(firstUserHobby)
+                    .uploadFile(userFile)
+                    .setCurrentAddress(firstUserAddress)
+                    .setStateAndCity(firstUserState, firstUserCity)
+                    .submitForm();
         });
 
         step("Check modal dialog (all fields)", () -> {
@@ -47,11 +47,11 @@ public class PracticeFormWithPageObjectsTest extends TestBase{
     void fillFormWithMinimumDataTest() {
 
         step("Fill form (minimum data)", () -> {
-                    practiceFormPage.openPage()
-                            .setName(secondUserFirstName, secondUserLastName)
-                            .setGender(secondUserGender)
-                            .setNumber(secondUserNumber)
-                            .submitForm();
+            practiceFormPage.openPage()
+                    .setName(secondUserFirstName, secondUserLastName)
+                    .setGender(secondUserGender)
+                    .setNumber(secondUserNumber)
+                    .submitForm();
         });
 
         step("Check modal dialog (minimum data)", () -> {
